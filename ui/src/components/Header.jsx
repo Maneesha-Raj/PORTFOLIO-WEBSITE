4a59457cd25dede259c65169e2800666a147a2db
Maneesha Raj <45869922+Maneesha-Raj@users.noreply.github.com>
2024-10-17 15:35:23 +0530

    Update Header.jsx

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -12,9 +12,9 @@ const Header = () => {
       <h1 className="text-6xl font-bold p-4"> MANEESHA <br /></h1>
       <h1 className="text-6xl font-bold p-4">RAJ<span className='text-fuchsia-500'>.</span></h1>
       <p className="text-2xl mt-4">Web Developer</p>
-     <button className="mt-6 px-6 py-2 bg-fuchsia-500 rounded-md text-white" >
+     <a href="https://github.com/Maneesha-Raj/maneesha-raj-portfolio.github.io/blob/main/ui/src/assets/cv/MANEESHA%20RAJ_Resume.pdf"><button className="mt-6 px-6 py-2 bg-fuchsia-500 rounded-md text-white" >
         Download Resume
-      </button>
+      </button></a>
     </header>
 
 
